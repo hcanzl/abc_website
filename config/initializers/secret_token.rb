86965e15cc1e1a4bc8a2ab4b4abd511fafa63d0d
Helen Canzler <helen.t.canzler@gmail.com>
2013-07-13 17:46:31 -0700

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbcWebsite::Application.config.secret_token = '046c8d302232dac50071bdfd3a118bfc9c82a85ed383af955e58291d8d48ee0d72933240239517c4a8ab0807e25d470bfd5adb1c41482ff21729c46f36fac19e'
