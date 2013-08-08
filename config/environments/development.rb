f8663f4b9799fb1284912356ea915206dbd3cd22
Helen Canzler <helen.t.canzler@gmail.com>
2013-08-07 23:38:51 -0700

    Use environment variables for smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,12 +19,12 @@ AbcWebsite::Application.configure do
   # Change mail delivery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               '',
-  user_name:            'sendtestproj',
-  password:             '#foobar#',
-  authentication:       'plain',
+  address:             'smtp.gmail.com',
+  port:                587,
+  domain:              '',
+  user_name:           ENV["GMAIL_USERNAME"],
+  password:            ENV["GMAIL_PASSWORD"],
+  authentication:     'plain',
   enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger
