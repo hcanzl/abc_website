9bc5c49cfc44f99864467f86898a10df851fde62
Helen Canzler <helen.t.canzler@gmail.com>
2013-08-12 22:27:19 -0700

    use environment variable for secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-#TODO: brakeman change to environment var, second add to .gitignore
-AbcWebsite::Application.config.secret_token = '046c8d302232dac50071bdfd3a118bfc9c82a85ed383af955e58291d8d48ee0d72933240239517c4a8ab0807e25d470bfd5adb1c41482ff21729c46f36fac19e'
+AbcWebsite::Application.config.secret_token = ENV["SECRET_TOKEN"]
